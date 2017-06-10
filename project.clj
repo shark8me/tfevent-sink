99c8d319f2374a572484025cdf0df083166a48a1
Kiran Karkera <kiran.karkera@gmail.com>
2017-06-10 11:31:16 +0800

    Version 0.1.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.shark8me/tfevent-sink "0.1.3-SNAPSHOT"
+(defproject org.shark8me/tfevent-sink "0.1.3"
   :description "Enables writing events from Clojure in Tensorflow format"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
